f78503544ca71ef6eec477ced056745fc73f2f83
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-06 00:20:14 +0530

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,9 +8,17 @@ const Table = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
 
+  const handleSearchChange = event => {
+    const value = event.target.value;
+    setCurrentPage(1); // Reset current page when search term changes
+    setSearchTerm(value);
+  };
+
   const filteredData = data.filter(item =>
-    item.id.includes(searchTerm) || item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    (typeof item.id === 'string' && item.id.includes(searchTerm)) ||
+    (typeof item.name === 'string' && item.name.toLowerCase().includes(searchTerm.toLowerCase()))
   );
+  
 
   const totalItems = filteredData.length;
   const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
@@ -22,55 +30,51 @@ const Table = () => {
 
   return (
     <>
-    
-    
-    <div className="overflow-x-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
-      <input
-        type="text"
-        placeholder="Search by USER ID or USER NAME"
-        className="border border-gray-300 px-3 py-2 rounded-md mb-4 w-full sm:w-auto"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-      <div className="shadow sm:rounded-lg">
-        <table className="w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                User ID
-              </th>
-              <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                User Name
-              </th>
-              <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Number of Quests
-              </th>
-              <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                XP
-              </th>
-              <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                $KIX Reward
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {currentItems.map(item => (
-              <tr key={item.id} className="hover:bg-gray-100">
-                <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.id}</td>
-                <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.name}</td>
-                <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.quest}</td>
-                <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.xp}</td>
-                <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.reward}</td>
+      <div className="overflow-x-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+        <input
+          type="text"
+          placeholder="Search by USER ID or USER NAME"
+          className="border border-gray-300 px-3 py-2 rounded-md mb-4 w-full sm:w-auto"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <div className="shadow sm:rounded-lg">
+          <table className="w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  User ID
+                </th>
+                <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  User Name
+                </th>
+                <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Number of Quests
+                </th>
+                <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  XP
+                </th>
+                <th scope="col" className="px-3 py-2 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  $KIX Reward
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {currentItems.map(item => (
+                <tr key={item.id} className="hover:bg-gray-100">
+                  <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.id}</td>
+                  <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.name}</td>
+                  <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.quest}</td>
+                  <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.xp}</td>
+                  <td className="px-3 py-2 sm:px-6 sm:py-4 whitespace-nowrap">{item.reward}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-  
-    </div>
 
-
-    <div className="flex justify-between mt-4">
+      <div className="flex justify-between mt-4">
         <button
           onClick={() => paginate(currentPage - 1)}
           disabled={currentPage === 1}
@@ -90,7 +94,6 @@ const Table = () => {
         </button>
       </div>
     </>
-
   );
 };
 
