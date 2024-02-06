49a32e47dfc082cc3bfa35532b0ab78260afca51
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-07 00:33:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container-wrapper mx-auto p-4">
         <Navbar />
         <h1 className=" text-2xl sm:text-4xl text-center mt-3 font-work text-white font-medium ">Zealy Sprint 1 Rewards</h1>
-        <h1 className=" text-lg sm:text-2xl text-center my-1 font-work text-white font-medium mb-4">35,000$KIX Total Prize Fund</h1>
+        <h1 className=" text-lg sm:text-2xl text-center my-3 font-work text-white font-medium mb-4">35,000$KIX Total Prize Fund</h1>
         <Table />
       </div>
     </>
