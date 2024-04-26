362edcedf85558bc374cc9759c6a620309e16c2c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-26 22:08:56 +0530

    new

--- src/Pages/Quest.jsx
+++ src/Pages/Quest.jsx
@@ -9,11 +9,18 @@ function Quest() {
     <div className="container-wrapper mx-auto p-4">
         <Navbar/>
         <h1 className="text-2xl sm:text-4xl text-center mt-3 font-work text-white font-medium">
-          Quest Sprint  Rewards
+        QuestN Sprint 1 Rewards
         </h1>
-        {/* <h1 className="text-lg sm:text-2xl text-center my-3 font-work text-white font-medium mb-4">
-          35,000$KIX Total Prize Fund
-        </h1> */}
+        <p className=' text-white text-sm text-center'>
+        (22.01.24 - 22.04.24)
+        </p>
+        <h1 className="text-lg sm:text-2xl text-center my-2 font-work text-white font-medium mb-1">
+        30,000 $KIX Total Prize Fund,
+        </h1>
+
+        <h1 className="text-lg sm:text-2xl text-center  font-work text-white font-medium mb-4">
+        100 $KIX per successful entrant
+        </h1>
 
         <div>
         <QuestTable/>
